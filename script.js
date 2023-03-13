ea415aa0e7cf28a851380e6928203336ce763de2
Tyrrance <tyrrancem37@gmail.com>
2023-03-13 17:59:42 -0400

    Set my variables for the password length, letters and special characters.

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-// Assignment Code
+var charlength = 8;
+var choiceArr = [];
+
+var specialCharArr =['&','@','#','!','%','^','^','*','(',')','~','.',',','?','{','}','[',']'];
+var lowerCassArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numbers = ['1','2','3','4','5','6','7','8','9','0'];
+
+
+
 var generateBtn = document.querySelector("#generate");
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +23,11 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  // PASSWORD GENERATED BASED ON PROMPT
+
+}
+
+function getPrompts(){
+
+}
